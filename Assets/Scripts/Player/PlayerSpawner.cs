85e7c4bfd7ccd604aa0b4b20a9e60fca726bd237
JackDanny64 <110450472+JackDanny64@users.noreply.github.com>
2026-03-07 22:28:40 +0200

    We have a Player 1 and player 2 prefab

--- Assets/Scripts/Player/PlayerSpawner.cs
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerSpawner : MonoBehaviour
 {
     public Transform[] Spawnpoints;
-    private int playercount;
+    public GameObject Player1Prefab;
+    public GameObject Player2Prefab;
+
+    private int playerCount = 0;
+    private PlayerInputManager manager;
+
+    void Start()
+    {
+        manager = PlayerInputManager.instance;
+
+        // First player will spawn this prefab
+        manager.playerPrefab = Player1Prefab;
+    }
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
-        playerInput.transform.position = Spawnpoints[playercount].transform.position;
-        playercount++;
+        // Move player to correct spawn point
+        playerInput.transform.position = Spawnpoints[playerCount].position;
+
+        playerCount++;
+
+        // Change prefab for the next player
+        if (playerCount == 1)
+        {
+            manager.playerPrefab = Player2Prefab;
+        }
     }
 }
